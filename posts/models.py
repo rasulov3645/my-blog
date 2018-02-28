060f5ddca0862c27bd7a8a857c200527bb6a7b77
Rasulov Gamzat <c0mrade3645@gmail.com>
2018-02-28 18:51:56 +0300

    add new app posts and user_profile, add model for this app, register in admin

--- posts/models.py
+++ posts/models.py
@@ -15,32 +15,20 @@ link:   https://djbook.ru/rel1.9/ref/models/fields.html
 
 '''
 
+from user_profile.models import User
 
 
-"""
-# Отношение многие-к-одному
-class School(models.Model):
-    pass
-
-class Student(models.Model):
-    school = models.ForeignKey(School)
-
-
-# Отношение один-к-одному
-class Entry(models.Model):
-    pass
-
-class EnttyDetail(models.Model):
-    entry = models.OneToOneField(Entry)
-    details = models.TextField()
+class Post(models.Model):
+    user = models.ForeignKey(User)
+    text = models.CharField(max_length=300)
+    created_date = models.DateTimeField(auto_now_add=True)
+    location = models.CharField(max_length=30, default='Global')
+    is_active = models.BooleanField(default=True)
+    is_favorite = models.BooleanField(default=False)
 
 
-# Отношение многие-ко-многим
-class Product(models.Model):
-    name = models.CharField()
+    def __str__(self):
+        return self.text[:50]
 
-class Category(models.Model):
-    name = models.CharField(
-        product = models.ManyToManyField('Product', blank=True, null=True),
-    )
-"""
+
+
