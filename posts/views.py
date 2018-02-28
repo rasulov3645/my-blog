08d024dc5e535109358ef0906d1ddf99c9b065c4
Rasulov Gamzat <c0mrade3645@gmail.com>
2018-02-28 12:13:20 +0300

    first view and add url

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+
+# home page
+def home(request):
+    context = '<h1>Hello world!</h1>'
+    return HttpResponse(context)
